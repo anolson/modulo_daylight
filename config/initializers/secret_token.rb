0f8dfac4e0272f297394c9423a92cdb6ebff2ffe
Andrew Olson <anolson@gmail.com>
2011-08-15 21:27:09 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModuloDaylight::Application.config.secret_token = '7568ff6eb4a5228dd753ad7b087b7bef1429a7049bbcb405b163b0f07a10088fd8d193bfdef96ff4bee044b1a5c3754a2a1b1afe28894fc160ec51c52e1b47be'
