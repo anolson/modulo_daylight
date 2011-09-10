924a89fef6485bcc09eccbf538befb4f92cc5b83
Andrew Olson <anolson@gmail.com>
2011-09-09 22:18:27 -0400

    Updating view.

--- app/helpers/locations/forecasts_helper.rb
+++ app/helpers/locations/forecasts_helper.rb
@@ -1,5 +1,9 @@
 module Locations::ForecastsHelper
-  def format_time(time)
-    time.strftime("%l:%M")
+  def sunset_in_words(time)
+    format = "%l:%M"
+    if(time.hour == 0)
+      format = "%k:%M"
+    end
+    time.strftime(format)
   end
 end
