8965a875ba6e2020aff5fd7c66d89a28b452d629
Andrew Olson <anolson@gmail.com>
2011-09-24 11:38:49 -0400

    Move time.rb to config/initializers

--- app/lib/time.rb
+++
@@ -1,9 +0,0 @@
-class Time
-  def round(seconds = 60)
-    Time.at((self.to_f / seconds).round * seconds)
-  end
-
-  def floor(seconds = 60)
-    Time.at((self.to_f / seconds).floor * seconds)
-  end
-end
