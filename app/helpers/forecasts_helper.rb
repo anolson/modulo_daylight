1ebedb12a96a5a63de1548e793c722e793213eac
Andrew Olson <anolson@gmail.com>
2011-09-24 20:31:19 -0400

    Don't round times.

--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -1,6 +1,6 @@
 module ForecastsHelper
   def format_time(time)
     # (time.hour == 0) && "%k:%M" || 
-    time.round.strftime((time.hour == 0) && "%k:%M" || "%l:%M").lstrip
+    time.strftime((time.hour == 0) && "%k:%M" || "%l:%M").lstrip
   end
 end
